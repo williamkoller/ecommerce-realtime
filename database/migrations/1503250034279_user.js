2d61c59acd6cb75d65bab08fab8288976647306a
William Koller <wkoller25@gmail.com>
2020-10-18 20:02:01 -0300

    feat: add migration user

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -7,9 +7,11 @@ class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments()
-      table.string('username', 80).notNullable().unique()
+      table.string('name', 80)
+      table.string('surname', 200)
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
+      table.integer('image_id').unsigned()
       table.timestamps()
     })
   }
