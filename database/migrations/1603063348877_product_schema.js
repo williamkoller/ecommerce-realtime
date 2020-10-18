53b444be32b14c91250ffb9d5221eca5cfc784a7
William Koller <wkoller25@gmail.com>
2020-10-18 20:41:29 -0300

    feat: add product and image relationships

--- database/migrations/1603063348877_product_schema.js
+++ database/migrations/1603063348877_product_schema.js
@@ -15,10 +15,19 @@ class ProductSchema extends Schema {
 
       table.foreign('image_id').references('id').inTable('images').onDelete('cascade')
     })
+
+    this.create('image_product', table => {
+      table.increments()
+      table.integer('image_id').unsigned()
+      table.integer('product_id').unsignde()
+      table.foreign('image_id').references('id').inTable('images').onDelete('cascade')
+      table.foreign('product_id').references('id').inTable('products').onDelete('cascade')
+    })
   }
 
   down() {
     this.drop('products')
+    this.drop('image_product')
   }
 }
 
