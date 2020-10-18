cc347615c6630c12dbd22214afb24d5b4377d042
William Koller <wkoller25@gmail.com>
2020-10-18 20:54:54 -0300

    feat: add relationships category and product

--- database/migrations/1603063348877_product_schema.js
+++ database/migrations/1603063348877_product_schema.js
@@ -19,15 +19,25 @@ class ProductSchema extends Schema {
     this.create('image_product', table => {
       table.increments()
       table.integer('image_id').unsigned()
-      table.integer('product_id').unsignde()
+      table.integer('product_id').unsigned()
       table.foreign('image_id').references('id').inTable('images').onDelete('cascade')
       table.foreign('product_id').references('id').inTable('products').onDelete('cascade')
     })
+
+    this.create('category_product', table => {
+      table.integer('product_id').unsigned()
+      table.integer('category_id').unsigned()
+
+      table.foreign('category_id').references('id').inTable('categories').onDelete('cascade')
+
+    })
   }
 
+
   down() {
-    this.drop('products')
+    this.drop('category_product')
     this.drop('image_product')
+    this.drop('products')
   }
 }
 
