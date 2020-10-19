2c3ebec6945d279662d623863a99d6800d8a38e4
William Koller <wkoller25@gmail.com>
2020-10-18 21:41:04 -0300

    feat: add migration order items

---
+++ database/migrations/1603067809499_order_item_schema.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class OrderItemSchema extends Schema {
+  up () {
+    this.create('order_items', (table) => {
+      table.increments()
+      table.integer('product_id').unsigned()
+      table.integer('quantity').unsigned()
+      table.decimal('subtotal', 12, 2)
+      table.integer('order_id').unsigned()
+
+      table.foreign('product_id').references('id').inTable('products').onDelete('cascade')
+      table.foreign('order_id').references('id').inTable('orders').onDelete('cascade')
+    })
+  }
+
+  down () {
+    this.drop('order_items')
+  }
+}
+
+module.exports = OrderItemSchema
