2b6057d2e4dcbb4f4476acbfa875a1ebf9307d17
William Koller <wkoller25@gmail.com>
2020-10-18 21:20:22 -0300

    feat: add migration coupon

---
+++ database/migrations/1603065572494_coupon_schema.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CouponSchema extends Schema {
+  up() {
+    this.create('coupons', (table) => {
+      table.increments()
+      table.string('code', 100).notBullable()
+      table.dateTime('valid_from')
+      table.dateTime('valid_until')
+      table.integer('quantity').defaultTo(1)
+      table.enu('can_use_for', ['product', 'client', 'product_client', 'all'])
+      table.enu('type', ['free', 'percent', 'currency']).defaultTo('currency')
+      table.boolen('recursive').defaultTo(false)
+      table.timestamps()
+    })
+  }
+
+  down() {
+    this.drop('coupons')
+  }
+}
+
+module.exports = CouponSchema
