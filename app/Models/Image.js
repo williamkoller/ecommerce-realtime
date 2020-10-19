15bee654d8087fcc89e335db42cce906169cbca5
William Koller <wkoller25@gmail.com>
2020-10-19 00:16:12 -0300

    feat: add Model Image

---
+++ app/Models/Image.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+const Env = use('Env')
+
+class Image extends Model {
+  static get computed () {
+    return ['url']
+  }
+  getUrl({ path }) {
+    return `${Env.get('APP_URL')}/images/${path}`
+  }
+}
+
+module.exports = Image
