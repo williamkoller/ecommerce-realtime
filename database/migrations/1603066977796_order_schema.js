b0b84583869d491abcf6136c4d3036f66c548bf4
William Koller <wkoller25@gmail.com>
2020-10-18 21:30:41 -0300

    feat: add migration order

---
+++ database/migrations/1603066977796_order_schema.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class OrderSchema extends Schema {
+  up () {
+    this.create('orders', (table) => {
+      table.increments()
+      table.decimal('total', 12, 2).defaultTo('0.0')
+      table.integer('user_id').unsigned()
+      table.enu('status', ['peding', 'cancelled', 'shipped', 'paid', 'finished'])
+      table.timestamps()
+
+      table.foreing('user_id').references('id').inTable('users').onDelete('cascade')
+    })
+  }
+
+  down () {
+    this.drop('orders')
+  }
+}
+
+module.exports = OrderSchema
