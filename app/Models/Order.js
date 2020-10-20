3bdcb2ccb9928e8738b02a817f0ff4c800d1f084
William Koller <wkoller25@gmail.com>
2020-10-20 01:05:53 -0300

    feat: add OrderHook

--- app/Models/Order.js
+++ app/Models/Order.js
@@ -4,6 +4,12 @@
 const Model = use('Model')
 
 class Order extends Model {
+  static boot() {
+    super.boot()
+    this.addHook('afterFind', 'OrderHook.updateValues')
+    this.addHook('afterPaginate', 'OrderHook.updateCollectionValues')
+  }
+
   items() {
     return this.hasMany('App/Models/OrderItem')
   }
