e6b0082f60dd4ac2d42658eb94163922375454ea
William Koller <wkoller25@gmail.com>
2020-10-20 01:47:49 -0300

    feat: add factory user, category and product

--- database/factory.js
+++ database/factory.js
@@ -12,10 +12,28 @@
 */
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
-// const Factory = use('Factory')
+const Factory = use('Factory')
 
-// Factory.blueprint('App/Models/User', (faker) => {
-//   return {
-//     username: faker.username()
-//   }
-// })
+Factory.blueprint('App/Models/User', faker => {
+  return {
+    name: faker.first({ nationality: 'us' }),
+    surname: faker.last(),
+    email: faker.email({ domain: 'gmail.com' }),
+    password: 'secret',
+  }
+})
+
+Factory.blueprint('App/Models/Category', faker => {
+  return {
+    title: faker.country({ full: true }),
+    description: faker.sentence(),
+  }
+})
+
+Factory.blueprint('App/Models/Product', faker => {
+  return {
+    name: faker.animal(),
+    description: faker.sentence(),
+    price: faker.floating({ min: 0, max: 1000, fixed: 2 }),
+  }
+})
