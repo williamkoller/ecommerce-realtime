b7b0198a66b903a48bf85e0616f4885314272be1
William Koller <wkoller25@gmail.com>
2020-10-21 00:33:24 -0300

    feat(controller): add order and product clitn controller

---
+++ app/Controllers/Http/Client/ProductController.js
@@ -0,0 +1,34 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with products
+ */
+class ProductController {
+  /**
+   * Show a list of all products.
+   * GET products
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index({ request, response, view }) {}
+
+  /**
+   * Display a single product.
+   * GET products/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show({ params, request, response, view }) {}
+}
+
+module.exports = ProductController
