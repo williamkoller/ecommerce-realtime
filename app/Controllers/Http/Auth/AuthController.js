4d885c1db35b6912a575912a867322a6e2cc1202
William Koller <wkoller25@gmail.com>
2020-10-21 01:35:19 -0300

    feat(controller): add method logout auth

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -40,7 +40,15 @@ class AuthController {
 
     return response.send({ data: user })
   }
-  async logout({ request, response, auth }) {}
+  async logout({ request, response, auth }) {
+    let refresh_token = request.input('refresh_token')
+    if (!refresh_token) {
+      refresh_token = request.header('refresh_token')
+    }
+
+    await auth.authenticator('jwt').revokeTokens([refresh_token], true)
+    return response.status(204).send({})
+  }
   async forgot({ request, response, auth }) {}
   async remember({ request, response }) {}
   async reset({ request, response }) {}
