bdc71822f6d5cb058c0f353fda5178a919430069
William Koller <wkoller25@gmail.com>
2020-10-21 00:18:43 -0300

    feat: add admin routes

---
+++ start/routes/admin.js
@@ -0,0 +1,35 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')}*/
+const Router = use('Route')
+/**
+ * Admin Routes
+ */
+Router.group(() => {
+  /**
+   * Categories resource routes
+   */
+  Router.resource('categories', 'CategoryController').apiOnly()
+  /**
+   * Coupon resource routes
+   */
+  Router.resource('coupons', 'CouponController').apiOnly()
+  /**
+   * Image resource routes
+   */
+  Router.resource('images', 'ImageController').apiOnly()
+  /**
+   * Order resource routes
+   */
+  Router.resource('orders', 'OrderController').apiOnly()
+  /**
+   * Products resource routes
+   */
+  Router.resource('products', 'ProductController').apiOnly()
+  /**
+   * User resource routes
+   */
+  Router.resource('users', 'UserController').apiOnly()
+})
+  .prefix('v1/admin')
+  .namespace('Admin')
--- start/routes/index.js
+++ start/routes/index.js
@@ -33,3 +33,4 @@ Route.get('/', () => {
 })
 
 require('./auth')
+require('./admin')
