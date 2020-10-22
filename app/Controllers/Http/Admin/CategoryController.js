5ccb90e1db62f06f67dbfef99df533f5e93d015b
William Koller <wkoller25@gmail.com>
2020-10-22 00:19:51 -0300

    feat(controller): implement method create category

--- app/Controllers/Http/Admin/CategoryController.js
+++ app/Controllers/Http/Admin/CategoryController.js
@@ -40,7 +40,22 @@ class CategoryController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store({ request, response }) {}
+  async store({ request, response }) {
+    try {
+      const { title, description, image_id } = request.all()
+      const categoryFound = await Category.findBy({ title })
+      if (categoryFound)
+        return response
+          .status(400)
+          .send({ message: 'This category already exists' })
+      const category = await Category.create({ title, description, image_id })
+      return response.status(201).send(category)
+    } catch (error) {
+      return response.status(400).send({
+        message: error.message,
+      })
+    }
+  }
 
   /**
    * Display a single category.
