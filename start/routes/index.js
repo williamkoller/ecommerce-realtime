504d5d8816b3de9996b9f94cdefc91474dffe743
William Koller <wkoller25@gmail.com>
2020-11-02 01:19:02 -0300

    refactor(middleware): add middleware on routes

--- start/routes/index.js
+++ start/routes/index.js
@@ -4,10 +4,10 @@ const Route = use('Route')
 Route.get('/', 'HomeController.index').namespace('Home')
 Route.get('/v1', () => {
   String.prototype.toHHMMSS = function () {
-    var sec_num = parseInt(this, 10) // don't forget the second param
-    var hours = Math.floor(sec_num / 3600)
-    var minutes = Math.floor((sec_num - hours * 3600) / 60)
-    var seconds = sec_num - hours * 3600 - minutes * 60
+    const sec_num = parseInt(this, 10) // don't forget the second param
+    let hours = Math.floor(sec_num / 3600)
+    let minutes = Math.floor((sec_num - hours * 3600) / 60)
+    let seconds = sec_num - hours * 3600 - minutes * 60
 
     if (hours < 10) {
       hours = '0' + hours
@@ -18,17 +18,17 @@ Route.get('/v1', () => {
     if (seconds < 10) {
       seconds = '0' + seconds
     }
-    var time = hours + ':' + minutes + ':' + seconds
+    const time = hours + ':' + minutes + ':' + seconds
     return time
   }
 
-  var time = process.uptime()
-  var uptime = (time + '').toHHMMSS()
+  const time = process.uptime()
+  const uptime = (time + '').toHHMMSS()
 
   return {
     greeting: 'Welcome to Ecommerce real time',
     version: process.version,
-    uptime: uptime,
+    uptime: uptime
   }
 })
 
