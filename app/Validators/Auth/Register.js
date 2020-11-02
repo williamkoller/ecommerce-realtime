ee9ba0b4236f8ff1b2a743b94747daf9d9af78f2
William Koller <wkoller25@gmail.com>
2020-11-02 02:51:40 -0300

    feat(validator): add validator messages Admin Register

--- app/Validators/Auth/Register.js
+++ app/Validators/Auth/Register.js
@@ -9,6 +9,18 @@ class Register {
       password: 'required|confirmed'
     }
   }
+
+  get messages() {
+    return {
+      'name.required': 'The name is required!',
+      'surname.required': 'The surnameis required!',
+      'email.required': 'The e-mail is required',
+      'email.email': 'The e-mail invalid',
+      'email.unique': 'This e-mail already exists',
+      'password.required': 'The password is required',
+      'password.confirmed': 'Passwords are not the same'
+    }
+  }
 }
 
 module.exports = Register
