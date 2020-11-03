4f2238b647968188831b7236e87778f08d013629
William Koller <wkoller25@gmail.com>
2020-11-02 23:17:46 -0300

    feat: add transformer product

---
+++ app/Transformers/Admin/ProductTransformer.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const BumblebeeTransformer = use('Bumblebee/Transformer')
+const ImageTransformer = use('App/Transformers/Admin/ImageTransformer')
+
+/**
+ * ProductTransformer class
+ *
+ * @class ProductTransformer
+ * @constructor
+ */
+class ProductTransformer extends BumblebeeTransformer {
+  static get defaultInclude() {
+    return ['image']
+  }
+  /**
+   * This method is used to transform the data.
+   */
+  transform(model) {
+    return {
+      id: model.id,
+      name: modelname,
+      description: model.description,
+      price: model.price
+    }
+  }
+
+  includeImage(model) {
+    return this.item(model.getRelated('image'), ImageTransformer)
+  }
+}
+
+module.exports = ProductTransformer
