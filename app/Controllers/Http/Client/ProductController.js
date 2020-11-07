7c357bfc32b20d4335a2e2eeb2726cbd7765be7d
William Koller <wkoller25@gmail.com>
2020-11-07 14:56:29 -0300

    feat: add list products for clients not auth

--- app/Controllers/Http/Client/ProductController.js
+++ app/Controllers/Http/Client/ProductController.js
@@ -2,11 +2,10 @@
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
 
-/**
- * Resourceful controller for interacting with products
- */
+const Product = use('App/Models/Product')
+const Transformer = use('App/Transformers/Admin/ProductTransformer')
+
 class ProductController {
   /**
    * Show a list of all products.
@@ -15,9 +14,33 @@ class ProductController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async index({ request, response, view }) {}
+  async index({ request, response, pagination, transform }) {
+    try {
+      const name = request.input('name')
+      if (!name) {
+        let products = await Product.query()
+          .whereNull('deleted_at')
+          .paginate(pagination.page, pagination.limit)
+
+        products = await transform.paginate(products, Transformer)
+        return response.status(200).send(products)
+      }
+
+      let query = await Product.query()
+        .where('name', 'ilike', `%${name}%`)
+        .paginate()
+
+      query = await transform.paginate(query, Transformer)
+
+      return response.status(200).send(query)
+    } catch (error) {
+      return response.status(400).send({
+        message: 'This request not performed',
+        error: error.stack
+      })
+    }
+  }
 
   /**
    * Display a single product.
@@ -26,9 +49,20 @@ class ProductController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async show({ params, request, response, view }) {}
+  async show({ params: { id }, response, transform }) {
+    try {
+      let product = await Product.findOrFail(id)
+
+      product = await transform.item(product, Transformer)
+      return response.status(200).send(product)
+    } catch (error) {
+      return response.status(400).send({
+        message: 'This request not performed',
+        error: error.stack
+      })
+    }
+  }
 }
 
 module.exports = ProductController
